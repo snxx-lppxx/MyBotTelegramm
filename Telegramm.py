f10af15c6b4b41ae7e5adfc47bf04c7fa51993dd
EstonianGuy <74711970+EstonianGuy@users.noreply.github.com>
2021-02-09 09:52:22 +0300

    Update Telegramm.py

--- Telegramm.py
+++ Telegramm.py
@@ -1,5 +1,5 @@
 import telebot
-bot = telebot.TeleBot('1532260141:AAFwqDh7CcnGh0bkhrX8PU8PNEQSV2H0EIs')
+bot = telebot.TeleBot('')
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
