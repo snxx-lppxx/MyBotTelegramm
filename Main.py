a47fc7bb4718ec65228bcc6ae73230a4818344cc
Kirill Kuzmin <kirik550088@gmail.com>
2021-02-13 11:52:27 +0300

    version 0.1 (checking for entering)

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
+import telebot
 import DataParser
 from DatabaseHandler import DatabaseHandler
 
+
+bot = telebot.TeleBot("TOKEN")
 
 def logic():
     # initialization
@@ -11,10 +14,32 @@ def logic():
     #     # err handling here
     #     return
     # bot greeting
-    input_book_name = input("Введите название произведения: ")
+
+# inputting for bot via function
+@bot.message_handler(regexp="SOME_REGEXP")
+def handle_message(check_book_name):
+    check_book_name = input("Введите название произведения: ")
+    pass
     # check exist in db
+    #write it by your own self
+
+    # review  these before coding
+    if (check_book_name == book_names):
+
+
+# code for mistake occassion
+   # else:
+        handle_message("Произведение, которое ты ввел, не существует в нашей базе данных")
+
+
+
     # do stuff
     pass
+
+# function for message in checking and other occassions
+@bot.message_handler(regexp="SOME_REGEXP")
+def handle_message(message):
+	pass
 
 
 if __name__ == "__main__":
